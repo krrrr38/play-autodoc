d5be37d4584f1e3401b4659c17cf304f893dc430
Hiromichi Kishi <kishi@interraps.com>
2015-10-08 23:15:23 +0900

    Added responseBodyParser option to handle expect String
    backport: https://github.com/krrrr38/play-autodoc/pull/2

--- play-autodoc-core/src/test/scala/com/krrrr38/play/autodoc/AutodocHelpersSpec.scala
+++ play-autodoc-core/src/test/scala/com/krrrr38/play/autodoc/AutodocHelpersSpec.scala
@@ -3,13 +3,14 @@ package com.krrrr38.play.autodoc
 import java.io.File
 
 import org.scalatest.{ BeforeAndAfterAll, FunSpec, Matchers }
-
+import play.api.libs.iteratee.{ Enumerator, Iteratee }
 import play.api.libs.json.Json
-import play.api.mvc._
-import play.api.test.FakeApplication
-
-import play.api.test._
+import play.api.mvc.{ Action, ResponseHeader, Result, Results }
 import play.api.test.Helpers._
+import play.api.test.{ FakeApplication, _ }
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 class AutodocHelpersSpec extends FunSpec with Matchers with BeforeAndAfterAll {
   val documentPath = "doc/com/krrrr38/play/autodoc/AutodocHelpers.md"
@@ -33,6 +34,13 @@ class AutodocHelpersSpec extends FunSpec with Matchers with BeforeAndAfterAll {
     case ("GET", "/api/users/yuno") =>
       Action { req =>
         Results.Ok(Json.obj("user" -> Json.obj("name" -> "yuno", "height" -> 144)))
+      }
+    case ("GET", "/api/bytes") =>
+      Action { req =>
+        Result(
+          header = ResponseHeader(200, Map(CONTENT_TYPE -> "application/x-bytes")),
+          body = Enumerator("bytes-data".getBytes)
+        )
       }
     case _ => throw new IllegalStateException("invalid routes")
   })
@@ -62,6 +70,26 @@ class AutodocHelpersSpec extends FunSpec with Matchers with BeforeAndAfterAll {
         contents should not include ("X-Secret-Key")
         contents should include("X-Api-Key: YOUR_API_KEY")
         contents should include("X-Public-Key: PUBLIC_KEY")
+      }
+    }
+
+    it("generate document in the case of byte array response") {
+      new WithServer(fakeApp) {
+        val req = FakeRequest("GET", "/api/bytes")
+        val res = AutodocHelpers.autodoc(
+          title = "GET /api/bytes",
+          responseBodyParser = (result: Result) => {
+            new String(Await.result(result.body |>>> Iteratee.consume[Array[Byte]](), Duration.Inf), "utf-8")
+          }
+        ).route(req).get
+        status(res) shouldBe OK
+        Thread.sleep(100)
+
+        val doc = new File(documentPath)
+        doc.exists() shouldBe true
+        val contents = scala.io.Source.fromFile(doc).getLines().mkString("\n")
+        contents should include("## GET /api/bytes")
+        contents should include("bytes-data")
       }
     }
   }
